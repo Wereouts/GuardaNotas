f325e4a8d3ffdfaad44b672640ca076926b0762b
Guilherme Costa Alves <galves@secullum.intra>
2025-12-22 15:08:00 -0300

    alterações

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const contador = document.querySelector(".quatidadeDeCaractere");
 const limite = 200;
 
 function atualizaValor() {
-  const valorInput = input.value.length;
-  contador.textContent = `${valorInput}/${limite}`;
+    const valorInput = input.value.length;
+    contador.textContent = `${valorInput}/${limite}`;
 }
 
 input.addEventListener("input", () => {
-  atualizaValor();
+    atualizaValor();
 });
 
 // ================================
@@ -22,52 +22,85 @@ input.addEventListener("input", () => {
 const pCard = document.querySelector(".textCard");
 
 function mudaValordoCard() {
-  const valorInput = input.value;
-  pCard.textContent = `${valorInput}`;
+    const valorInput = input.value;
+    pCard.textContent = `${valorInput}`;
 }
 
 // ================================
 // ADIÇÃO DE CARDS
 // ================================
 
-const btnAdicionar = document.querySelector(".botaoAdicionar");
 const container = document.querySelector(".CardPrincipal");
-  let totalCards = 0;
-  const LIMITE = 5;
 
-  btnAdicionar.addEventListener("click", () => {
+let totalCards = 0;
+const LIMITE = 5;
+
+function verificaButton() {
+    const valorInput = input.value;
+
+    if (valorInput.length === 0) {
+        return;
+    }
+
     if (totalCards >= LIMITE) {
-      alert("Voce pode adicionar só até 5 cards");
-      return;
+        alert("Voce pode adicionar só até 5 cards");
+        return;
     }
 
     container.insertAdjacentHTML(
-      "beforeend",
-      `
-        <div class="cardGeral">
+        "beforeend",
+        `
+        <div class="cardGeral" id="id-${totalCards}">
           <div class="cards">
-            <p class="textCard"></p>
+            <p class="textCard">${valorInput}</p>
           </div>
           <div class="acoes">
-            <button class="btnEditar">Editar</button>
-            <button class="btnExcluir">Excluir</button>
+            <button class="btnEditar" onclick="editaCard(this)">Editar</button>
+            <button class="btnExcluir" onclick="excluirCard(this)">Excluir</button>
           </div>
         </div>
       `
     );
+    input.value = "";
+    atualizaValor();
     totalCards++;
-  });
+};
 
-// ================================
-// VERIFICAÇÃO DE BOTÃO
-// ================================
+function editaCard(botaoClicado) {
 
-function verificaButton() {
-  const valor = Number(pCard.textContent);
+    const card = botaoClicado.closest(".cardGeral");
 
-  if (valor === 0) {
-    mudaValordoCard();
-  } else {
-    adicionaCard();
-  }
+    const paragrafo = card.querySelector(".textCard");
+    paragrafo.contentEditable = "true";
+    paragrafo.focus();
+
+
+}
+
+
+
+function excluirCard(botaoClicado) {
+    const card = botaoClicado.closest(".cardGeral");
+    if (card) {
+        card.remove();
+    }
+
+
+}
+
+function excluirTodosCards () {
+    const confirmar = confirm("Tem certeza que deseja excluir todos os cards?")
+    if (!confirmar) {
+        return;
+    }
+
+    const todosCards = document.querySelectorAll('.cardGeral');
+
+    todosCards.forEach(card => {
+        card.remove();
+
+    });
+
+    totalCards = 0;
+        
 }
